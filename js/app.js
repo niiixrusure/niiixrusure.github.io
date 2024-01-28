7f4113e4a41fb71134fc85ca37693814dfed6bf9
Ilyamori <157722098+Ilyamori@users.noreply.github.com>
2024-01-28 16:09:46 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ document.addEventListener('mousemove', e => {
 		`
 	})
 })
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
