01a8676ac0ce608db84d719c53fb022413009931
Ilya <157722098+Ilyamori@users.noreply.github.com>
2024-11-05 00:26:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
