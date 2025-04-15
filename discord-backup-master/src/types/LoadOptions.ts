8e3e28f2e42b9ad54061e266348c2ad7ffb99712
Ilya <157722098+Ilyamori@users.noreply.github.com>
2025-04-15 05:33:09 +0400

    Delete discord-backup-master directory

--- discord-backup-master/src/types/LoadOptions.ts
+++
@@ -1,7 +0,0 @@
-import { MessageMentionOptions } from "discord.js";
-
-export interface LoadOptions {
-    clearGuildBeforeRestore: boolean;
-    maxMessagesPerChannel?: number;
-    allowedMentions?: MessageMentionOptions;
-}
