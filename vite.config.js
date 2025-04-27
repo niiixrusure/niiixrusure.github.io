e7f8c6fb94624b5a26c54d670235d984ed4c006b
niiixrusure <nklu66423@gmail.com>
2025-04-27 13:06:45 +0300

    sdf

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/niiixrusure.github.io",
+  base: "/intun-page",
   plugins: [react()],
 });
